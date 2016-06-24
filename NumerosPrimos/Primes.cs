2e283b556462f8b1315122436d167b6bbb594a1e
Ronald Albert <ronaldalbert1609@gmail.com>
2016-06-24 11:09:26 -0300

    TrabalhodeNumerosPrimos

---
+++ NumerosPrimos/Primes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPrimos
+{
+    static class Primes
+    {
+        public static void Primos(int range)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+            HashSet<int> primes =  new HashSet<int>();
+            for (int i = 2; i < range; i++)
+            {
+                numbers.Add(i);
+                primes.Add(i);
+            }
+            foreach(int i in numbers)
+            {
+                foreach(int j in numbers)
+                {
+                    if(j % i == 0 && i != j)
+                    {
+                        primes.Remove(j);
+                    }
+                }
+            }
+            foreach(int i in primes)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
